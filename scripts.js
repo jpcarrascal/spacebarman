03ff266b93b00bf616c82aa21256bbb22017cb71
JP <jp@JP-M1-1424.local>
2024-07-30 08:49:17 -0500

    Separated social links, added merch store

--- scripts.js
+++ scripts.js
@@ -1,38 +1,41 @@
 const links = [
-    {"include":true,  "expires": "2024-06-13 10:30:00", name:"event",    text:"Sonar demo: Count-Me-In", link:"https://count-me-in.azurewebsites.net/sequencer?room=sonarintro", icon:""},
-    {"include":true,  "expires": "2024-06-13 10:30:00", name:"event",    text:"Sonar demo: DBASS", link:"https://dbass.link/sequencer?room=sonarintro", icon:""},
-    {"include":true,  "expires": "2024-06-21 08:30:00", name:"event",    text:"Concierto: Los Leales + Spacebarman", link:"https://dice.fm/event/l8abbp-los-leales-arg-spacebarman-21st-jun-sala-taro-barcelona-tickets", icon:""},
-    {"include":true,  "expires": "no", name:"spotify",    text:"", link:"https://open.spotify.com/album/1NBr6aZbhbvWyyQJ3THhzP?si=a4uTPQ5oQjGoQdS2TyFzHw", icon:"logo-spotify.png"},
-    {"include":true,  "expires": "no", name:"instagram", text:"", link:"https://www.instagram.com/spacebarman", icon:"logo-instagram.png"},
-    {"include":true,  "expires": "no", name:"youtube",    text:"", link:"https://youtube.com/spacebarmanvideo", icon:"logo-youtube.png"},
-    {"include":true,  "expires": "no", name:"bandcamp",   text:"", link:"https://spacebarman.bandcamp.com/album/el-ansia", icon:"logo-bandcamp.png"},
-    {"include":true,  "expires": "no", name:"soundcloud", text:"", link:"https://soundcloud.com/spacebarman/sets/el-ansia", icon:"logo-soundcloud.png"},
-    {"include":false, "expires": "no", name:"deezer", text:"", link:"https://www.deezer.com/en/artist/4246882", icon:"logo-deezer.png"},
-    {"include":false, "expires": "no", name:"amazon", text:"", link:"https://music.amazon.com/artists/B009VU5FNW/spacebarman", icon:"logo-amazon.png"},
-    {"include":false, "expires": "no", name:"applumusic", text:"", link:"https://music.apple.com/us/artist/spacebarman/525428256", icon:"logo-applemusic.png"},
-    {"include":true,  "expires": "no", name:"bio", text:"Bio", link:"bio/", icon:""},
-    {"include":true,  "expires": "no", name:"facebook", text:"Facebook", link:"https://www.facebook.com/spacebarman", icon:""},
-    {"include":true,  "expires": "no", name:"subscribe", text:"Subscribe to our mailing list", link:"https://spacebarman.us21.list-manage.com/subscribe?u=eebe2ae40abe6e1bfafe680d8&id=8652641ff4", icon:""}
+    {"include":"links-main",  "expires": "2024-06-13 10:30:00", name:"event",    text:"Sonar demo: Count-Me-In", link:"https://count-me-in.azurewebsites.net/sequencer?room=sonarintro", icon:""},
+    {"include":"links-main",  "expires": "2024-06-13 10:30:00", name:"event",    text:"Sonar demo: DBASS", link:"https://dbass.link/sequencer?room=sonarintro", icon:""},
+    {"include":"links-main",  "expires": "2024-06-21 08:30:00", name:"event",    text:"Concierto: Los Leales + Spacebarman", link:"https://dice.fm/event/l8abbp-los-leales-arg-spacebarman-21st-jun-sala-taro-barcelona-tickets", icon:""},
+    {"include":"links-social",  "expires": "no", name:"spotify",    text:"", link:"https://open.spotify.com/album/1NBr6aZbhbvWyyQJ3THhzP?si=a4uTPQ5oQjGoQdS2TyFzHw", icon:"logo-spotify-small.png"},
+    {"include":"links-social",  "expires": "no", name:"instagram", text:"", link:"https://www.instagram.com/spacebarman", icon:"logo-instagram-small.png"},
+    {"include":"links-social",  "expires": "no", name:"youtube",    text:"", link:"https://youtube.com/spacebarmanvideo", icon:"logo-youtube-small.png"},
+    {"include":"links-main",  "expires": "no", name:"bandcamp",   text:"", link:"https://spacebarman.bandcamp.com/album/el-ansia", icon:"logo-bandcamp.png"},
+    {"include":"links-main",  "expires": "no", name:"soundcloud", text:"", link:"https://soundcloud.com/spacebarman/sets/el-ansia", icon:"logo-soundcloud.png"},
+    {"include":"links-main",  "expires": "no", name:"merch", text:"Merch", link:"https://www.stickermule.com/u/nevergetridofnoise", icon:""},
+    {"include":"none", "expires": "no", name:"deezer", text:"", link:"https://www.deezer.com/en/artist/4246882", icon:"logo-deezer.png"},
+    {"include":"none", "expires": "no", name:"amazon", text:"", link:"https://music.amazon.com/artists/B009VU5FNW/spacebarman", icon:"logo-amazon.png"},
+    {"include":"none", "expires": "no", name:"applumusic", text:"", link:"https://music.apple.com/us/artist/spacebarman/525428256", icon:"logo-applemusic.png"},
+    {"include":"links-main",  "expires": "no", name:"bio", text:"Bio", link:"bio/", icon:""},
+    {"include":"none",  "expires": "no", name:"facebook", text:"Facebook", link:"https://www.facebook.com/spacebarman", icon:""},
+    {"include":"links-main",  "expires": "no", name:"subscribe", text:"Subscribe to our mailing list", link:"https://spacebarman.us21.list-manage.com/subscribe?u=eebe2ae40abe6e1bfafe680d8&id=8652641ff4", icon:""}
 ];
 
 window.onload = function() {
-
     for(var i=0; i<links.length; i++) {
+        var container = document.querySelector('#' + links[i].include);
         var localTime = Date.now();
-        if(links[i].expires !== "no") {
+        console.log(container)
+        if(links[i].expires !== "no" && container) {
             var expires = Date.parse(links[i].expires);
             console.log("expires: " + (expires - localTime));
             if(expires > localTime)
-                addLink(links[i]);
-        } else {
-            addLink(links[i]);
+                addLink(links[i], container);
+        } else if(container) {
+            addLink(links[i], container);
         }
     }
+
 };
 
-function addLink(link) {
-    if(link.include) {
-        var linksContainer = document.querySelector('#links-container');
+function addLink(link, container) {
+    if(link.include == container.id) {
+        var linksContainer = container;
         var linkDiv = document.createElement('div');
         linkDiv.setAttribute('class', 'link-item');
         if(link.text != "")  {
